7747fe17330c74f2ae59d64fe009b34c36998bb4
Tekendra Bhandari <tekendraa67@gmail.com>
2025-04-17 20:41:54 +0545

    sggd

--- 4th batch/x_to_pow_y.c
+++ 4th batch/x_to_pow_y.c
@@ -1,23 +1,26 @@
 // WAP in C to calculate x to the power of y without using pow() function.
 #include <stdio.h>
 
-int main() 
-{
-    int x, y, i, result = 1;
-    printf("Enter the base number (x): ");
-    scanf("%d", &x);
-    printf("Enter the exponent (y): ");
-    scanf("%d", &y);
-    if (y < 0) 
-    {
-        printf("Error! Exponent cannot be negative.");
-    } else 
-    {
-        for (i = 1; i <= y; i++) 
-        {
-            result = result * x;
-        }
-        printf("%d to the power of %d is %d.", x, y, result);
-    }
+// Recursive function to calculate x^y
+int power(int x, int y) {
+    if (y == 0)
+        return 1;
+    else
+        return x * power(x, y - 1);
+}
+
+int main() {
+    int base, exponent, result;
+
+    printf("Enter base (x): ");
+    scanf("%d", &base);
+    
+    printf("Enter exponent (y): ");
+    scanf("%d", &exponent);
+
+    result = power(base, exponent);
+
+    printf("%d to the power of %d is %d\n", base, exponent, result);
+
     return 0;
 }
