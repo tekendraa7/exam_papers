75eb6f8b6fe41c8549d83a756388c3204c689df1
Tekendra Bhandari <tekendraa67@gmail.com>
2025-04-17 14:32:52 +0545

    fdxbfxdbf

--- 1st batch/ascii.c
+++ 1st batch/ascii.c
@@ -2,21 +2,22 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
     char str[100];
     int i = 0;
 
-    
     printf("Enter a string: ");
-    scanf("%[^\n]", str); 
+    scanf("%[^\n]", str);
 
-    
-    while (str[i] != '\0') {
-       
-        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || 
+    while (str[i] != '\0')
+    {
+
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
             str[i] == 'o' || str[i] == 'u' ||
             str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
-            str[i] == 'O' || str[i] == 'U') {
+            str[i] == 'O' || str[i] == 'U')
+        {
             printf("Character: %c, ASCII: %d\n", str[i], str[i]);
         }
         i++;
